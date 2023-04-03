c5dcfc60c3a7858924bc5548c4903e8acdbee574
aakihabara <113220235+aakihabara@users.noreply.github.com>
2023-04-03 21:42:41 +1100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace Matematiko
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(475, 164);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(219, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Начать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 20.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Location = new System.Drawing.Point(80, 35);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView1.Size = new System.Drawing.Size(350, 350);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseClick);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.ColumnHeadersVisible = false;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 20.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView2.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView2.Location = new System.Drawing.Point(732, 35);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersVisible = false;
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 29;
+            this.dataGridView2.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView2.Size = new System.Drawing.Size(350, 350);
+            this.dataGridView2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(475, 266);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(219, 57);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Рестарт";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Location = new System.Drawing.Point(200, 410);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 57);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox2.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox2.Location = new System.Drawing.Point(862, 410);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 57);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.textBox3.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox3.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox3.Location = new System.Drawing.Point(548, 58);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(70, 57);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1158, 669);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,408 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Matematiko
+{
+    public partial class Form1 : Form
+    {
+        bool start = false;
+        List<int> nums = new List<int>();
+        int maxn = 51;
+        int curr, step, next, currptsP, currptsB;
+        bool firststep = true;
+        Random rnd = new Random();
+        int newbotpts = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.Width = 350;
+            dataGridView1.Height = 350;
+            dataGridView2.Width = 350;
+            dataGridView2.Height = 350;
+            dataGridView1.RowCount = 5;
+            dataGridView1.ColumnCount = 5;
+            dataGridView2.RowCount = 5;
+            dataGridView2.ColumnCount = 5;
+
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                dataGridView1.Rows[i].Height = 70;
+                dataGridView1.Columns[i].Width = 70;
+                dataGridView1.Rows[i].DefaultCellStyle.BackColor = Color.Gray;
+                dataGridView1.Rows[i].DefaultCellStyle.ForeColor = Color.White;
+            }
+            for (int i = 0; i < dataGridView2.RowCount; i++)
+            {
+                dataGridView2.Rows[i].Height = 70;
+                dataGridView2.Columns[i].Width = 70;
+                dataGridView2.Rows[i].DefaultCellStyle.BackColor = Color.Gray;
+                dataGridView2.Rows[i].DefaultCellStyle.ForeColor = Color.White;
+            }
+            for(int i = 1; i < 14; i++) // Заполнение списка колоды. 
+            {
+                for(int j = 0; j < 4; j++)
+                {
+                    nums.Add(i);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) // Кнопка начала игры.
+        {
+            button1.Visible = false;
+            start = true;
+            step = rnd.Next(0, maxn + 1);
+            next = nums[step];
+            nums.RemoveAt(step);
+            maxn--;
+            textBox3.Text = next.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) // Снять выделение при каждом нажатии на форму
+        {
+            dataGridView1.ClearSelection();
+            dataGridView2.ClearSelection();
+        }
+
+        public void RobStep() // Алгоритм бота.
+        {
+            int botpts = Check(dataGridView2);
+            int X = 0, Y = 0, botold = newbotpts;
+
+            if(firststep)
+            {
+                dataGridView2[0, 0].Value = curr;
+                return;
+            }
+
+            for(int i = 0; i < dataGridView2.RowCount; i++)
+            {
+                for(int j = 0; j < dataGridView2.ColumnCount; j++)
+                {
+                    if(dataGridView2[i,j].Value == null)
+                    {
+                        dataGridView2[i, j].Value = curr;
+                        newbotpts = Check(dataGridView2);
+                        if(newbotpts > botpts)
+                        {
+                            botpts = newbotpts;
+                            X = i;
+                            Y = j;
+                        }
+                        dataGridView2[i, j].Value = null;
+                    }
+                }
+            }
+
+            if (botold == newbotpts)
+            {
+                randomstep(ref X, ref Y);
+                while (dataGridView2[X, Y].Value != null)
+                {
+                    randomstep(ref X, ref Y);
+                }
+            }
+
+            if (dataGridView2[X, Y].Value != null)
+            {
+                RobStep();
+            }
+            else
+            {
+                dataGridView2[X, Y].Value = curr;
+            }
+            
+        }
+
+        public void randomstep(ref int a, ref int b) // Выбор случайной клетки на поле.
+        {
+            a = rnd.Next(0, 5);
+            b = rnd.Next(0, 5);
+        }
+
+        public bool final() // Проверка на заполненность полей.
+        {
+            for(int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                for(int j = 0; j < dataGridView1.ColumnCount; j++)
+                {
+                    if(dataGridView1[i,j].Value == null)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public void restart() // Пересоздание игры.
+                              // Установка начальных значений для переменных и полей.
+        {
+            nums = new List<int>();
+            firststep = true;
+            for (int i = 1; i <= 13; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    nums.Add(i);
+                }
+            }
+            start = true;
+            maxn = 51;
+            step = rnd.Next(1, maxn);
+            next = nums[step];
+            nums.RemoveAt(step);
+            maxn--;
+            textBox3.Text = next.ToString();
+            textBox1.Text = null;
+            textBox2.Text = null;
+
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                {
+                    dataGridView1[i, j].Value = null;
+                    dataGridView2[i, j].Value = null;
+                }
+            }
+        }
+
+        public int resreturn(int[] a, bool b) // Подсчёт очков в строке, столбце или диагонали.
+        {
+            int res = 0;
+
+            if((a[0] == 3) && ( a[12] == 2))
+            {
+                if (b)
+                {
+                    res += 110;
+                }
+                else
+                {
+                    res += 100;
+                }
+            }
+
+            if ((a[0] != 0) && (a[12] != 0) && (a[11] != 0) && (a[10] != 0) && (a[9] != 0))
+            {
+                if (b)
+                {
+                    res += 160;
+                }
+                else
+                {
+                    res += 150;
+                }
+            }
+
+            if (a[0] == 4)
+            {
+                if(b)
+                {
+                    res += 210;
+                }
+                else
+                {
+                    res += 200;
+                }
+            }
+
+            for(int i = 0; i < 13; i++)
+            {
+                if(a[i] == 2)
+                {
+                    if (b)
+                    {
+                        res += 20;
+                    }
+                    else
+                    {
+                        res += 10;
+                    }
+                    for(int k = 0; k < 13; k++)
+                    {
+                        if(k != i)
+                        {
+                            if(a[k] == 2)
+                            {
+                                if (b)
+                                {
+                                    res += 30;
+                                }
+                                else
+                                {
+                                    res += 20;
+                                }
+                            }
+                            if(a[k] == 3)
+                            {
+                                if (b)
+                                {
+                                    res += 90;
+                                }
+                                else
+                                {
+                                    res += 80;
+                                }
+                            }
+                        }
+                    }
+                }
+                if(a[i] == 3)
+                {
+                    if (b)
+                    {
+                        res += 50;
+                    }
+                    else
+                    {
+                        res += 40;
+                    }
+                }
+                if (a[i] == 4)
+                {
+                    if (b)
+                    {
+                        res += 170;
+                    }
+                    else
+                    {
+                        res += 160;
+                    }
+                }
+            }
+
+            for(int i = 0; i < 9; i ++)
+            {
+                if(a[i] * a[i+1] * a[i+2] * a[i+3] * a[i+4] != 0)
+                {
+                    if (b)
+                    {
+                        res += 60;
+                    }
+                    else
+                    {
+                        res += 50;
+                    }
+                }
+            }
+
+            return res;
+        }
+
+        private void button2_Click(object sender, EventArgs e) // Кнопка рестарта.
+        {
+            restart();
+        }
+
+        public int Check(DataGridView a) // Создание массива с числами от 1 до 13, в котором в каждом элементе
+                                         // содержится количество того или иного числа в строке, столбце или диагонали,
+                                         // а затем вызов метода подсчёта очков.
+        {
+            int[] num = new int[13];
+            int ptssum = 0;
+            int finalres = 0;
+            for(int i = 0; i < a.RowCount; i++)
+            {
+                for(int j = 0; j < a.ColumnCount; j++)
+                {
+                    if(a[i, j].Value != null)
+                    {
+                        num[int.Parse(a[i, j].Value.ToString()) - 1]++;
+                    }
+                }
+                ptssum = resreturn(num, false);
+                finalres += ptssum;
+                num = new int[13];
+            }
+            for (int i = 0; i < a.RowCount; i++)
+            {
+                for (int j = 0; j < a.ColumnCount; j++)
+                {
+                    if (a[j, i].Value != null)
+                    {
+                        num[int.Parse(a[j, i].Value.ToString()) - 1]++;
+                    }
+                }
+                ptssum = resreturn(num, false);
+                finalres += ptssum;
+                num = new int[13];
+            }
+            for(int i = 0; i < a.RowCount; i++)
+            {
+                if (a[i, i].Value != null)
+                {
+                    num[int.Parse(a[i, i].Value.ToString()) - 1]++;
+                }
+            }
+            ptssum = resreturn(num, true);
+            finalres += ptssum;
+            return finalres;
+        }
+
+        private void dataGridView1_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e) // При каждом нажатии на какую-то ячейку поля
+                                                                                                   // на него заносится текущее число колоды, производится ход бота и в
+                                                                                                   // textbox'ы выводится количество очков двух полей.
+        {
+            step = rnd.Next(1, maxn);
+            dataGridView1.ClearSelection();
+            dataGridView2.ClearSelection();
+            if (start == true)
+            {
+                if(dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value == null)
+                {
+                    curr = int.Parse(textBox3.Text);
+                    next = nums[step];
+                    nums.RemoveAt(step);
+                    dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = curr;
+                    maxn--;
+                    RobStep();
+                    maxn--;
+                    currptsP = Check(dataGridView1);
+                    currptsB = Check(dataGridView2);
+                    textBox1.Text = currptsP.ToString();
+                    textBox2.Text = currptsB.ToString();
+                    firststep = false;
+                    if (final())
+                    {
+                        textBox3.Text = "";
+                        int p1 = Check(dataGridView1);
+                        int p2 = Check(dataGridView2);
+                        textBox1.Text = p1.ToString();
+                        textBox2.Text = p2.ToString();
+                        if (p1 > p2)
+                        {
+                            MessageBox.Show("Победил игрок!");
+                            restart();
+                        }
+                        else if (p1 < p2)
+                        {
+                            MessageBox.Show("Победил компьютер!");
+                            restart();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Ничья!");
+                            restart();
+                        }
+                    }
+                    else
+                    {
+                        textBox3.Text = next.ToString();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Ячейка занята!");
+                }
+            }
+        }
+    }
+}
